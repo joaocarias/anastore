19380fd62e027d7264bdd1fcb825167b746b1ade
Joao Carias <l4154@liga.org.br>
2020-06-28 15:36:19 -0300

    criando autenticacao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ana Store
                 </div>
 
                 <div class="links">
